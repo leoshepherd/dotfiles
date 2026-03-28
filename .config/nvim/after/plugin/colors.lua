86f039c4dd32919a085d273e095bc609c959d4e6
Bailey Shepherd <baileyray8@aol.com>
2026-03-28 01:19:34 -0600

    new dual-boot-branch

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,14 +1,9 @@
-function ColorMyPencils()
-    color = color or "onenord"
-    vim.cmd.colorscheme(color)
+vim.cmd.colorscheme("tokyonight")
 
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalSB", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NeotreeNormal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NeotreeNormalNC", { bg = "none" })
-end
-
-ColorMyPencils()
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalSB", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NeotreeNormal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NeotreeNormalNC", { bg = "none" })
