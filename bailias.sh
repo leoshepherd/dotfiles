2f5d212cb8ab5a232e71d8272da4434a39603f8d
Shepherd <bshepherd@ws-cvvl724.corp.icr-team.com>
2026-04-07 11:08:56 -0600

    new prompt

--- bailias.sh
+++ bailias.sh
@@ -1,9 +1,26 @@
-export PS1='\[\033[36m\] \u\[\033[32m\]  \h:\[\033[31;1m\]  \w\[\033[37m\]$(parse_git_branch)\[\033[m\]\n '
+source ~/dotfiles/prompt/pastel.sh
+export PS1=$'$(_fg ${USER_BG})$(_fg ${USER_TEXT})$(_bg ${USER_BG}) \u$(_fg ${USER_BG})$(_bg ${DIR_BG})$(_fg ${DIR_TEXT})  \w$(_fg ${DIR_BG})$(_bg ${SEP_BG1})$(_fg ${SEP_BG1})$(_bg ${SEP_BG2})$(_fg ${SEP_BG2})$(_bg ${SEP_BG3})$(_fg ${SEP_BG3})$(_bg ${GIT_BG})$(_fg ${GIT_TEXT})$(parse_git_branch)\e[0m$(_fg ${GIT_BG})\e[0m\n '
 
 alias vi='nvim'
-alias gs='git status'
+alias gs='git status -sb'
 alias gc='git checkout'
 alias ll='ls -la --color=auto'
+
+_fg() {
+    hex=${1#\#}
+    r=$((16#${hex:0:2}))
+    g=$((16#${hex:2:2}))
+    b=$((16#${hex:4:2}))
+    printf "\e[38;2;%d;%d;%dm" $r $g $b
+}
+
+_bg() {
+    hex=${1#\#}
+    r=$((16#${hex:0:2}))
+    g=$((16#${hex:2:2}))
+    b=$((16#${hex:4:2}))
+    printf "\e[48;2;%d;%d;%dm" $r $g $b
+}
 
 grs() {
     git reset --soft HEAD~$1
