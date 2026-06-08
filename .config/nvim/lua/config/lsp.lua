1dac8e70a0e1fe43f94ac461734042b7ff2783f9
Bailey Shepherd <baileyray8@aol.com>
2026-06-08 01:04:14 -0600

    rust lsp and code comp

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -1,9 +1,13 @@
 if vim.fn.executable('java') == 1 then
-    vim.lsp.enable({'java'})
+    vim.lsp.enable({ 'java' })
 end
 
-if vim.fn.executable( 'lua-language-server' ) == 1 then
-    vim.lsp.enable({'lua'})
+if vim.fn.executable('lua-language-server') == 1 then
+    vim.lsp.enable({ 'lua' })
+end
+
+if vim.fn.executable('rust-analyzer') == 1 then
+    vim.lsp.enable({ 'rust' })
 end
 
 vim.diagnostic.config({
@@ -31,32 +35,32 @@ vim.diagnostic.config({
 
 
 vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(args)
-    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-    vim.keymap.set("n", "gd", function()
-        vim.cmd("vsplit")
-        vim.lsp.buf.definition()
-    end, {silent=true, noremap=true})
+    callback = function(args)
+        local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+        vim.keymap.set("n", "gd", function()
+            vim.cmd("vsplit")
+            vim.lsp.buf.definition()
+        end, { silent = true, noremap = true })
 
-    if client.supports_method('textDocument/completion') then
-        local trigger = {};
-        for i = 32, 126 do
-            table.insert(trigger, string.char(i))
+        if client:supports_method('textDocument/completion') then
+            local trigger = {};
+            for i = 32, 126 do
+                table.insert(trigger, string.char(i))
+            end
+            client.server_capabilities.completionProvider.triggerCharacters = trigger
+
+            vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
         end
-        client.server_capabilities.completionProvider.triggerCharacters = trigger
 
-        vim.lsp.completion.enable(true, client.id, args.buf, {autotrigger = true})
-    end
-
-    if not client.supports_method('textDocument/willSaveWaitUntil')
-        and client.supports_method('textDocument/formatting') then
-        vim.api.nvim_create_autocmd('BufWritePre', {
-            group = vim.api.nvim_create_augroup('my.lsp', {clear=false}),
-            buffer = args.buf,
-            callback = function()
-                vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
-            end,
-        })
-    end
-  end,
+        if not client:supports_method('textDocument/willSaveWaitUntil')
+            and client:supports_method('textDocument/formatting') then
+            vim.api.nvim_create_autocmd('BufWritePre', {
+                group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
+                buffer = args.buf,
+                callback = function()
+                    vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
+                end,
+            })
+        end
+    end,
 })
